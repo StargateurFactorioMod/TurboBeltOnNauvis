57a8e647b8b8b56f9a5b9e390617c0e294dc905f
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2026-01-02 17:12:07 +0100

    0.3.2

--- data-updates.lua
+++ data-updates.lua
@@ -10,7 +10,6 @@ local function find_item(entity_name)
   end
 end
 
-
 ---@param words string[]
 ---@param x string
 local function any_of(words, x)
@@ -26,40 +25,45 @@ local words = {
   "underground",
   "splitter",
   "loader",
+  "lane%-splitter",
 }
 
 for _, recipe in pairs(data.raw["recipe"]) do
   local item = table.deepcopy(find_item(recipe.name))
   if item and recipe.name:find("turbo") and recipe.category ~= "recycling" and any_of(words, recipe.name) then
     local uranium_recipe = table.deepcopy(recipe)
+    local find_tungsten_plate = false
     for _, ingredients in ipairs(uranium_recipe.ingredients) do
       if ingredients.name == "tungsten-plate" then
         ingredients.name = "uranium-238"
+        find_tungsten_plate = true
         break
       end
     end
-    uranium_recipe.localised_name = { "",
-      recipe.localised_name or { string.format("entity-name.%s", uranium_recipe.name) },
-      " ", { "item-name.uranium-238" },
-    }
-    uranium_recipe.name = uranium_recipe.name .. "-uranium-238"
-    uranium_recipe.category = "crafting-with-fluid-or-metallurgy"
-    uranium_recipe.surface_conditions = nil
+    if find_tungsten_plate then
+      uranium_recipe.localised_name = { "",
+        recipe.localised_name or { string.format("entity-name.%s", uranium_recipe.name) },
+        " ", { "item-name.uranium-238" },
+      }
+      uranium_recipe.name = uranium_recipe.name .. "-uranium-238"
+      uranium_recipe.category = "crafting-with-fluid-or-metallurgy"
+      uranium_recipe.surface_conditions = nil
 
-    local icons = item.icons or { { icon = item.icon, icon_size = item.icon_size } }
-    table.insert(icons, {
-      icon = "__base__/graphics/icons/uranium-238.png",
-      icon_size = 64,
-      scale = 1 / 4,
-      shift = { 8, 8 },
-    })
-    uranium_recipe.icons = icons
-    table.insert(data.raw.technology["turbo-transport-belt-uranium-238"].effects, {
-      type = "unlock-recipe",
-      recipe = uranium_recipe.name,
-    })
-    table.insert(uranium_recipes, uranium_recipe)
-  end
+      local icons = item.icons or { { icon = item.icon, icon_size = item.icon_size } }
+      table.insert(icons, {
+        icon = "__base__/graphics/icons/uranium-238.png",
+        icon_size = 64,
+        scale = 1 / 4,
+        shift = { 8, 8 },
+      })
+      uranium_recipe.icons = icons
+      table.insert(data.raw.technology["turbo-transport-belt-uranium-238"].effects, {
+        type = "unlock-recipe",
+        recipe = uranium_recipe.name,
+      })
+      table.insert(uranium_recipes, uranium_recipe)
+    end
+    end
 end
 
 data:extend(uranium_recipes)
